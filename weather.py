4df4daa9f30c81124b9f5ccebffbcdc6e9ee3c93
Caden <93957669+heqds@users.noreply.github.com>
2022-02-03 09:56:09 -0500

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,9 +1,10 @@
 import requests
 
-API_KEY = "APIKEYHERE"
+API_KEY = "API_KEY_HERE"
 BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
 
 city = input("Enter a city name: ")
+print ("------------------------")
 request_url = f"{BASE_URL}?appid={API_KEY}&q={city}"
 response = requests.get(request_url)
 
@@ -12,9 +13,14 @@ if response.status_code == 200:
     weather = data['weather'][0]['description']
     temperature = round(data["main"]["temp"] - 273.15)
     feels_like = round(data["main"]["feels_like"] - 273.15)
+    temp_min = round(data["main"]["temp_min"] - 273.15)
+    temp_max = round(data["main"]["temp_max"] - 273.15)
 
     print("Weather:", weather)
     print("Temperature:", temperature, "celsius")
     print("Feels like:", feels_like)
+    print("Temperature minimum:", temp_min)
+    print("Temperature max", temp_max)
+
 else:
-    print("Error.")
+    print("Error please enter a valid city.")
